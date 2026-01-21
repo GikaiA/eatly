4c1737129ae936cbbb1cb765d1b662e20642aeaf
Gikai Andrews <gnaboys@gmail.com>
2026-01-20 21:59:21 -0500

    added questions for questions component

--- eatly/src/Questions/Questions.jsx
+++ eatly/src/Questions/Questions.jsx
@@ -16,6 +16,24 @@ function Questions() {
       </div>
       <br></br>
       <h3>How much time do you have?</h3>
+      <div className="button-group">
+        <button>Quick bite (under 30 mins)</button>
+        <button>Moderate (30 mins - 1 hour)</button>
+        <button>Leisurely (over 1 hour)</button>
+      </div>
+      <h3>What's your budget?</h3>
+      <div className="button-group">
+        <button>Quick bite (under 30 mins)</button>
+        <button>Moderate (30 mins - 1 hour)</button>
+        <button>Leisurely (over 1 hour)</button>
+      </div>
+       <h3>Who are you dining with?</h3>
+      <div className="button-group">
+        <button>Solo</button>
+        <button>Partner/Date</button>
+        <button>Family/Friends</button>
+        <button>Large group</button>
+      </div>
     </div>
   );
 }
