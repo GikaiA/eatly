b2289d5be54155ebccd2235b3be867c5dd9a3e81
Gikai Andrews <gnaboys@gmail.com>
2026-02-06 21:23:37 -0500

    added frontend to login, added extra picture, created profile component skeleton

--- eatly/src/Login/Login.jsx
+++ eatly/src/Login/Login.jsx
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
 import "./Login.css";
 import { auth } from "../firebase";
-import { signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import {
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 
 function Login() {
@@ -25,42 +29,53 @@ function Login() {
 
   const handleGoogleSignIn = async () => {
     const provider = new GoogleAuthProvider();
-    
+
     try {
       await signInWithPopup(auth, provider);
       navigate("/questions");
     } catch (error) {
-      setError('Google sign-in failed');
+      setError("Google sign-in failed");
       console.error("Google sign-in failed: ", error);
     }
   };
 
   return (
-    <div className="login">
-      <form onSubmit={handleLogin} className="login-form">
-        <h1>Login to Eatly</h1>
-        <label>Username or Email</label>
-        <input
-          type="text"
-          placeholder="Username or Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <label>Password</label>
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button className="sign-in-button">Sign In</button>
-        {/* <hr className="dashed">OR</hr> */}
-        <button onClick={handleGoogleSignIn}> Sign In With Google</button>
-        <p>
-         Don't have an account? Click <a href="/register">here</a>
-        </p>
-      </form>
-    </div>
+    <>
+      <div className="split left">
+        <p className="login-image">an image will be placed here</p>
+      </div>
+      <div className="split right">
+        <div className="login">
+          <form onSubmit={handleLogin} className="login-form">
+            <h1>Login to Eatly</h1>
+            <label>Username or Email</label>
+            <input
+              type="text"
+              placeholder="Username or Email"
+              className="login-field"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <label>Password</label>
+            <input
+              type="password"
+              placeholder="Password"
+              className="login-field"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button className="sign-in-button">Sign In</button>
+            <div className="divider">
+              <span>OR</span>
+            </div>
+            <button onClick={handleGoogleSignIn} className="google-button"> Sign In With Google</button>
+            <p className="register-link">
+              Don't have an account? Click <a href="/register">here</a>
+            </p>
+          </form>
+        </div>
+      </div>
+    </>
   );
 }
 
